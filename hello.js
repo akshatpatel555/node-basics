0d0fece4cb06f8e57069fce63e44a956ed48cff3
Rob Hess <hessro@oregonstate.edu>
2021-11-04 16:12:39 -0700

    Add code from 11/4 lecture.

---
+++ hello.js
@@ -0,0 +1,9 @@
+console.log("Hello world!!")
+console.log("== __dirname:", __dirname)
+console.log("== __filename:", __filename)
+// console.log("== process:", process)
+
+// var nameOfVariable = "value of variable"
+console.log("== process.env:", process.env)
+console.log("== process.env.USER:", process.env.USER)
+console.log("== process.env.DB_USERNAME:", process.env.DB_USERNAME)
